38958c6abf7c2dff47879b1dacc016fe2461192e
StrangerZhang <fangyizhangz@gmail.com>
2018-10-11 11:01:46 +0800

    fix seed for reproduction

--- siamfc/train.py
+++ siamfc/train.py
@@ -24,6 +24,8 @@ from .alexnet import SiameseAlexNet
 from .dataset import ImagnetVIDDataset
 from .custom_transforms import Normalize, ToTensor, RandomStretch, RandomCrop, CenterCrop
 
+torch.manual_seed(1234)
+
 def train(gpu_id, data_dir):
     # loading meta data
     meta_data_path = os.path.join(data_dir, "meta_data.pkl")
@@ -115,4 +117,5 @@ def train(gpu_id, data_dir):
             summary_writer.add_scalar('valid/loss', valid_loss, (epoch+1)*len(trainloader))
 
             torch.save(model.cpu().state_dict(), "./models/siamfc_{}.pth".format(epoch+1))
+            model.cuda()
             scheduler.step()
